6ca1fee64cc7c4fed655c6da60dac0d6793f311a
ayalena <82506563+ayalena@users.noreply.github.com>
2022-01-13 10:26:31 +0100

    first commit

---
+++ src/main/java/com/eindproject/YogaShare/YogaShareApplication.java
@@ -0,0 +1,13 @@
+package com.eindproject.YogaShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogaShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogaShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eindproject/YogaShare/YogaShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eindproject.YogaShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogaShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
