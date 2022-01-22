83b3dc23c717c54af885df10e2973804c51e1cf8
ayalena <82506563+ayalena@users.noreply.github.com>
2022-01-22 15:21:15 +0100

    tests for file service

---
+++ src/test/java/com/eindproject/YogaShare/files/FileDBServiceTest.java
@@ -0,0 +1,56 @@
+package com.eindproject.YogaShare.files;
+
+import com.eindproject.YogaShare.exceptions.RecordNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+class FileDBServiceTest {
+
+    @Mock FileDBRepository fileDBRepository;
+
+    @InjectMocks
+    private FileDBService fileDBService;
+
+    @Mock
+
+    FileDB fileDB;
+
+    @Test
+    public void getFileByIdTest() {
+        fileDB = new FileDB();
+        fileDB.setId(1L);
+        Mockito
+                .when(fileDBRepository.findById(1L))
+                .thenReturn(Optional.of(fileDB));
+        FileDB found = fileDBService.getFileById(1L);
+        assertEquals(1L, found.getId());
+    }
+
+    @Test
+    public void getFileByIdExceptionTest() {
+        assertThrows(RecordNotFoundException.class, () -> fileDBService.getFileById(null));
+    }
+
+    @Test
+    public void deleteFileTest() {
+        fileDB = new FileDB();
+        fileDB.setId(1L);
+        Mockito
+                .when(fileDBRepository.existsById(1L))
+                .thenReturn(true);
+        fileDBService.deleteFile(1L);
+        verify(fileDBRepository, times(1)).deleteById(fileDB.getId());
+        assertThat(fileDB.getId()).isEqualTo(1L);
+    }
+}
